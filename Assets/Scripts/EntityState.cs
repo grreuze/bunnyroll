66b1326d889940c45ccedcdf6ef72dd9d5f7b091
grreuze <grreuze@gmail.com>
2018-03-14 23:17:25 +0100

    ready to version control

---
+++ Assets/Scripts/EntityState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct EntityState {
+
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public EntityState(Vector3 position, Quaternion rotation) {
+		this.position = position;
+		this.rotation = rotation;
+	}
+	
+}
