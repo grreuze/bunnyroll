00de633143c832ffdd22f9fb526348d58c6f1950
grreuze <grreuze@gmail.com>
2018-04-06 17:54:22 +0200

    wow il faut merge

--- Assets/Plugins/HierarchyDisabler/HierarchyDisabler.cs
+++ Assets/Plugins/HierarchyDisabler/HierarchyDisabler.cs
@@ -1,17 +1,25 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 
 [ExecuteInEditMode]
 public class HierarchyDisabler : MonoBehaviour {
 
 	public static HierarchyDisabler[] all;
-	
-	private void OnEnable() {
-		all = FindObjectsOfType<HierarchyDisabler>();
 
-		foreach (HierarchyDisabler hd in all) {
-			if (hd != this)
-				hd.gameObject.SetActive(false);
+	enum Type {
+		Nothing, DisableAllOthers
+	}
+	[SerializeField] Type onEnable;
+
+	private void OnEnable() {
+		if (onEnable == Type.DisableAllOthers) {
+			all = FindObjectsOfType<HierarchyDisabler>();
+
+			foreach (HierarchyDisabler hd in all) {
+				if (hd != this)
+					hd.gameObject.SetActive(false);
+			}
 		}
 	}
-	
 }
+#endif
